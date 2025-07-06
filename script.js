6be1ab7aede13aa280d0ee5d5bd86f58fcf7d7f9
Christopherles <lestinchristopher@gmail.com>
2025-07-06 17:38:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// Génération des étoiles
+// étoiles scintillantes
 for (let i = 0; i < 100; i++) {
-  const star = document.createElement('div');
+  let star = document.createElement('div');
   star.className = 'etoile';
-  const size = Math.random() * 2 + 1;
+  let size = Math.random() * 1.5 + 0.5;
   star.style.width = star.style.height = `${size}px`;
   star.style.top = `${Math.random() * window.innerHeight}px`;
   star.style.left = `${Math.random() * window.innerWidth}px`;
   document.body.appendChild(star);
 }
 
-// Génération des étoiles filantes
+// étoiles filantes
 for (let i = 0; i < 5; i++) {
-  const meteor = document.createElement('div');
+  let meteor = document.createElement('div');
   meteor.className = 'etoile-filante';
   meteor.style.top = `${Math.random() * 300}px`;
   meteor.style.left = `${Math.random() * window.innerWidth}px`;
-  meteor.style.animationDelay = `${Math.random() * 10}s`;
+  meteor.style.animationDelay = `${Math.random() * 15}s`;
   document.body.appendChild(meteor);
 }
